94c824531b4fc6e7ef4168fe1051af6bd7fa9c3b
AmanRohi <=>
2024-12-09 16:36:48 +0530

    project completed

---
+++ index.js
@@ -0,0 +1,71 @@
+
+require('dotenv').config();
+const mongoose=require("mongoose");
+const express = require('express');
+const { ethers } = require("ethers");
+const FormData = require('form-data');
+const axios  = require('axios');
+const cors = require("cors")
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(
+    cors({
+      origin: "*",
+      credentials: true, //access-control-allow-credentials:true
+      optionSuccessStatus: 200,
+    })
+  )
+
+
+
+
+
+app.post('/send_data', async (req, res) => {
+  // IPFS API endpoint
+  const ipfsApiUrl = 'http://127.0.0.1:5001/api/v0/add';
+
+  // JSON data to store
+  const data = req.body;
+  data["timestamp"] = Date.now();
+  console.log(data);
+
+  try {
+      // Create a FormData instance
+      const formData = new FormData();
+
+      // Add the JSON data as a string
+      formData.append('file', JSON.stringify(data));
+
+      // Send POST request to IPFS API
+      const response = await axios.post(ipfsApiUrl, formData, {
+          headers: formData.getHeaders(),
+      });
+
+      if (response.status === 200) {
+          console.log(`JSON data stored in IPFS with CID: ${response.data.Hash}`);
+          res.json({ "Ipfs-Hash": response.data.Hash });
+      } else {
+          console.error(`Failed to upload data: ${response.statusText}`);
+          res.json({ "Ipfs-Hash": "None" });
+      }
+  } catch (error) {
+      console.error(`Error uploading data to IPFS: ${error.message}`);
+      res.json({ "Ipfs-Hash": "None" });
+  }
+});
+
+const PORT=3002;
+app.set("port", process.env.port || 3002)
+app.listen(app.get("port"), async() => {
+  try{     
+    console.log(`Notification service running on port ${PORT}`)
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDbConnected`);
+    }
+    catch(error){
+        console.log("Unsucess :"+error);
+    }
+});
